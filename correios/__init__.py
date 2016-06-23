36e6effc715d3999be0d58309f44a1b36eb6c8a7
Osvaldo Santana Neto <github@osantana.me>
2016-06-22 23:16:39 -0300

    Include datadir in sdist distribution

--- correios/__init__.py
+++ correios/__init__.py
@@ -11,3 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+import os
+
+
+DATADIR = os.path.join(os.path.dirname(__file__), "data")
