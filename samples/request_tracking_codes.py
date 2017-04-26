8fb9f6b53b8f11c4d656d31d91be3119303942a2
Osvaldo Santana Neto <osantana@users.noreply.github.com>
2017-04-26 11:54:03 -0300

    Remove trailing whitespaces

--- samples/request_tracking_codes.py
+++ samples/request_tracking_codes.py
@@ -23,6 +23,6 @@ def get_tracking_codes(service, quantity):
     tracking_codes = client.request_tracking_codes(olist_user, Service.get(service), quantity=quantity)
     print(tracking_codes)
 
-    
+
 get_tracking_codes('41068', 1)  # Request 1 PAC Tracking Code
 get_tracking_codes('40068', 1)  # Request 1 SEDEX Tracking Code
