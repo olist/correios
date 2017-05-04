768fc5c959e9f2b96de99ecc4f5ed56a9a811867
Osvaldo Santana Neto <github@osantana.me>
2017-05-04 18:56:57 -0300

    Bump version: 1.1.2 → 2.0.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requirements = load_requirements("requirements.txt")
 
 setup(
     name="correios",
-    version="1.1.2",
+    version="2.0.0",
     url="https://github.com/osantana/correios",
 
     author="Osvaldo Santana Neto",
