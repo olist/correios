6df4593c28567b745baacde45b82b0422b6e4997
Bruno Soares <brunomsss@gmail.com>
2018-06-06 14:25:25 -0300

    Bump version: 4.6.0 → 4.6.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requirements = load_requirements("requirements.txt")
 
 setup(
     name="correios",
-    version="4.6.0",
+    version="4.6.1",
     url="https://github.com/osantana/correios",
 
     author="Osvaldo Santana Neto",
