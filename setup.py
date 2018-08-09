b57aca8af7ed610992dfa2096b29a4a006a11c0b
Marcelo Belli <marcelo@belli.me>
2018-08-09 15:00:03 -0300

    Bump version: 4.8.0 → 5.0.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requirements = load_requirements("requirements.txt")
 
 setup(
     name="correios",
-    version="4.8.0",
+    version="5.0.0",
     url="https://github.com/osantana/correios",
 
     author="Osvaldo Santana Neto",
