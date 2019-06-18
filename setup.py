3f29534c5b078d141ef0c0fdad96ed5df0a321ac
João Luiz Lorencetti <joao.lorencetti@olist.com>
2019-06-18 15:49:41 -0300

    Bump version: 6.1.15 → 6.1.16

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requirements = load_requirements("requirements.txt")
 
 setup(
     name="correios",
-    version="6.1.15",
+    version="6.1.16",
     url="https://github.com/osantana/correios",
     author="Osvaldo Santana Neto",
     author_email="correiospy@osantana.me",
