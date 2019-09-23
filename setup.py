8116d91d8be203a37b5cf9af268ce44d090acb28
Christian Hartung <christian.hartung@olist.com>
2019-09-23 13:07:57 -0300

    Bump version: 6.1.20 → 6.1.21

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requirements = load_requirements("requirements.txt")
 
 setup(
     name="correios",
-    version="6.1.20",
+    version="6.1.21",
     url="https://github.com/osantana/correios",
     author="Osvaldo Santana Neto",
     author_email="correiospy@osantana.me",
