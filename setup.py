e0a3c61647781a3b1c90c163658fcee95b0c399f
Danilo Shiga <daniloshiga@gmail.com>
2019-10-14 15:02:14 -0300

    Bump version: 6.1.22 → 6.1.23

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requirements = load_requirements("requirements.txt")
 
 setup(
     name="correios",
-    version="6.1.22",
+    version="6.1.23",
     url="https://github.com/osantana/correios",
     author="Osvaldo Santana Neto",
     author_email="correiospy@osantana.me",
