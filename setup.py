39a607c1a3bae5557fb2773b3eda132032ddd4b0
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-27 10:22:42 -0300

    Bump version: 6.4.3 → 6.4.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requirements = load_requirements("requirements.txt")
 
 setup(
     name="correios",
-    version="6.4.3",
+    version="6.4.4",
     url="https://github.com/osantana/correios",
     author="Osvaldo Santana Neto",
     author_email="correiospy@osantana.me",
