56e4bc210e27848d7a8034b3216a6a76f1f25789
Affonso Azevedo <contato.affonsobrian@gmail.com>
2020-09-15 16:16:26 -0300

    Bump version: 6.4.4 → 6.4.5

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requirements = load_requirements("requirements.txt")
 
 setup(
     name="correios",
-    version="6.4.4",
+    version="6.4.5",
     url="https://github.com/osantana/correios",
     author="Osvaldo Santana Neto",
     author_email="correiospy@osantana.me",
